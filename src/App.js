59596f302f667b6bee1cb82713e627c75a58e888
rraj-03 <ritikraj1369@gmail.com>
2023-03-06 21:58:12 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import SponsorsPage from './Components/SponsorsPage';
 import IndustryPage from './Components/IndustryPage';
 import { useEffect, useState } from 'react';
@@ -166,40 +166,55 @@ function App() {
         });
       }
   }, [])
+  let location = useLocation()
+  useEffect(() => {
+  console.log('location.pathname', location.pathname)
+  document.querySelectorAll(".nav-link").forEach((ele)=>{
+    ele.classList.remove("active")
+  })
+  document.getElementById(location.pathname.slice(1)+"-").classList.add("active")
+  }, [location.pathname])
+  
   return (
     <div className="App">
       <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
+      <h1 class="logo me-auto"><Link to="/">Arsha</Link></h1>
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="/#heroes">Home</a></li>
-          <li><a class="nav-link scrollto" href="/#about">About</a></li>
-          <li><a class="nav-link scrollto" href="/#timeline">Timeline</a></li>
-          <li><a class="nav-link scrollto" href="/#testimonials">Testimonials</a></li>
-          <li><a class="nav-link   scrollto" href="/spons">Sponsors</a></li>
-          <li><a class="nav-link scrollto" href="/teams">Team</a></li>
-          <li class="dropdown"><a href="/industry"><span>Industry</span> </a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+          <li class="dropdown"><Link class="nav-link scrollto active" to="/" id='-'>Home</Link>
+          <ul>
+          <li><Link class="nav-link scrollto" to="/#about">About</Link></li>
+          <li><Link class="nav-link scrollto" to="/#timeline">Timeline</Link></li>
+          <li><Link class="nav-link scrollto" to="/#testimonials">Testimonials</Link></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+          
+          
+          
+          <li><Link class="nav-link scrollto" to="/spons" id='spons-'>Sponsors</Link></li>
+          <li><Link class="nav-link scrollto" to="/teams" id='teams-'>Team</Link></li>
+          <li class="dropdown"><Link to="/industry" className='nav-link scrollto' id='industry-'>Industry </Link>
+            <ul>
+              <li><Link to="#">Drop Down 1</Link></li>
+              <li class="dropdown"><Link to="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></Link>
+                <ul>
+                  <li><Link to="#">Deep Drop Down 1</Link></li>
+                  <li><Link to="#">Deep Drop Down 2</Link></li>
+                  <li><Link to="#">Deep Drop Down 3</Link></li>
+                  <li><Link to="#">Deep Drop Down 4</Link></li>
+                  <li><Link to="#">Deep Drop Down 5</Link></li>
+                </ul>
+              </li>
+              <li><Link to="#">Drop Down 2</Link></li>
+              <li><Link to="#">Drop Down 3</Link></li>
+              <li><Link to="#">Drop Down 4</Link></li>
+            </ul>
+          </li>
+          {/* <li><Link class="nav-link scrollto" to="#contact">Contact</Link></li> */}
+          <li><Link class="getstarted scrollto" to="/#about">Get Started</Link></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
@@ -216,7 +231,88 @@ function App() {
     <Route exact path='/teams' element={< TeamAndContact/>}/>
     <Route exact path='/industry' element={<IndustryPage setTitle={setTitle} setp1={setp1} setp2={setp2} setbutton1={setbutton1} setbutton2={setbutton2} setbuttonname1={setbuttonname1} setbuttonname2={setbuttonname2}/>} />
 
-    </Routes>      
+    </Routes> 
+    <footer id="footer">
+{/* <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Join Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"/><input type="submit" value="Subscribe"/>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div> */}
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Arsha</h3>
+            <p>
+              A108 Adam Street <br/>
+              New York, NY 535022<br/>
+              United States <br/><br/>
+              <strong>Phone:</strong> +1 5589 55488 55<br/>
+              <strong>Email:</strong> info@example.com<br/>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Social Networks</h4>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        {/* <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ --> */}
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+</footer>
     </div>
   );
 }
